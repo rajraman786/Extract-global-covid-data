9a8a8e24c6238058e7ab9b5a5e6e68059ed641c4
RAMAN RAJ <rajraman786@yahoo.com>
2021-06-20 00:34:39 +0530

    gui added to project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Extract Covid-Data
 
-The corona virus is one of the biggest pandemics that put the world at risk. At the same time, this is one of the most trending messages that all these days have. In this article, we will Extract data and provide Covid-19 statistics in a human-readable form and store the data in an Excel sheet. You can scrap the data with [this link](https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/)
+The corona virus is one of the biggest pandemics that put the world at risk. At the same time, this is one of the most trending messages that all these days have. In this article, we will Extract data and provide Covid-19 statistics in a human-readable form and store the data in an Excel sheet. You can scrap the data with [this link](https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/) . we have also build its gui using tkinter which recieve country name as input from user and give output as conformed case , death no. and continent in which country belongs .
 
 
 ## Contributing
@@ -19,10 +19,17 @@ Install the required libraries .
   pip install bs4
   pip install texttable
   pip install requests
+  pip install tkinter
 ```
     
 ## License
 
 [MIT](https://choosealicense.com/licenses/mit/)
+
+  
+## Screenshots
+
+
+<p align="center"><img src="ss.jpg"></p>
 
   
--- pro.py
+++ pro.py
@@ -1,6 +1,10 @@
 import requests
 import texttable
+import csv
+from tkinter import *
+import tkinter.messagebox
 from bs4 import BeautifulSoup 
+
 url = 'https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/'
 # get URL html 
 page = requests.get(url) 
@@ -24,8 +28,74 @@ while True:
 	except StopIteration: 
 		break
 # Sort the data by the number of confirmed cases 
-data.sort(key = lambda row: row[1], reverse = True) 
+data.sort(key = lambda row: row[1], reverse = True)
 
+def take_data(x):
+	
+	for i in range(220):
+	
+		if(data[i][0]==x):
+			
+			a=data[i][1]
+			b=data[i][2]
+			c=data[i][3]
+			break
+	return(a,b,c)
+
+#code for gui starts here
+
+def reset():
+    country_name.set("")
+    case_da.delete(0,END)
+    death_da.delete(0,END)
+    continent_da.delete(0,END)
+
+def extract():
+    
+    if (country_name.get()==""):
+        tkinter.messagebox.showerror("Error","country name not enterred\n")
+    else:
+        ca,de,co=take_data(country_name.get())
+        case_da.insert(0,str(ca))
+        death_da.insert(0,str(de))
+        continent_da.insert(0,str(co))
+
+
+root = Tk()
+root.geometry("600x600")
+root.minsize(600,600)
+root.maxsize(600, 600)
+root.title("Extracting Covid Data")
+photo = PhotoImage(file="MASK.png")
+Label(root,image=photo).place(x=0,y=0)
+
+Label(root, text="For Extracting Covid Data \n Enter Country Name", font="comicsansms 15 bold", pady=15, bg='yellow').place(x=170,y=30)
+Label(root, text="Extract", font="comicsansms 15 bold", pady=15, bg='yellow').place(x=260,y=200)
+Label(root, text="Cases", font="comicsansms 15 bold", pady=15, bg='yellow').place(x=50,y=300)
+Label(root, text="Death", font="comicsansms 15 bold", pady=15, bg='yellow').place(x=250,y=300)
+Label(root, text="Continent", font="comicsansms 15 bold", pady=15, bg='yellow').place(x=450,y=300)
+
+
+country_name = StringVar()
+Entry(root, textvariable=country_name).place(x=230,y=150)
+case_da=Entry(root)
+case_da.place(x=20,y=400)
+death_da=Entry(root)
+death_da.place(x=220,y=400)
+continent_da=Entry(root)
+continent_da.place(x=440,y=400)
+#Creates button attached with extract function
+extract_da = Button(root,font=('arial',15,'bold'), text = "Extract",pady=15, bg='green', command=extract) 
+extract_da.place(x=250,y=200)
+
+#Creates button attached with reset function
+reset_da = Button(root,font=('arial',15,'bold'), text = "Reset", bg ='red', command=reset) 
+reset_da.place(x=250,y=500)
+
+
+root.mainloop()
+
+#code for gui ends here
 
 # create texttable object 
 table = texttable.Texttable() 
@@ -33,9 +103,9 @@ table.add_rows([(None, None, None, None)] + data)  # Add an empty row at the beg
 table.set_cols_align(('c', 'c', 'c', 'c'))  # 'l' denotes left, 'c' denotes center, and 'r' denotes right 
 table.header((' Country ', ' Number of cases ', ' Deaths ', ' Continent ')) 
   
-print(table.draw())
-import csv
-# header for csv file ..
+#print(table.draw())
+
+
 fields = ['Country','Cases','Death','Continent']
 # opening and creating a csv file :
 with open('mydata.csv', 'w') as file:
